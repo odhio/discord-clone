0ee04053cf3a076787793aca2f1763b732ceab70
odhio <odai@intersect.inc>
2024-01-04 05:35:16 +0900

    server-modals

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,7 +2,15 @@
 
 import { useEffect, useState } from 'react'
 
+import { CreateChannelModal } from '@/components/modals/create-channel-modal'
 import { CreateServerModal } from '@/components/modals/create-server-modal'
+import { DeleteChannelModal } from '@/components/modals/delete-channel-modal'
+import { DeleteServerModal } from '@/components/modals/delete-server-modal'
+import { EditChannelModal } from '@/components/modals/edit-channel-modal'
+import { EditServerModal } from '@/components/modals/edit-server-modal'
+import { InviteModal } from '@/components/modals/invite-modal'
+import { LeaveServerModal } from '@/components/modals/leave-server-modal'
+import { MembersModal } from '@/components/modals/members-modal'
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -18,6 +26,14 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
+      <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
+      <DeleteChannelModal />
+      <EditChannelModal />
     </>
   )
 }
