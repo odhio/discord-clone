6bc73f29b2fda83297fb9fb08213aa54f0c4ef6d
odhio <odai@intersect.inc>
2024-01-04 04:14:39 +0900

    uploadthing-setup

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 
+import { FileUpload } from '@/components/file-upload'
 import { Button } from '@/components/ui/button'
 import {
   Dialog,
@@ -87,6 +88,24 @@ export const InitialModal = () => {
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+
               <FormField
                 control={form.control}
                 name="name"
